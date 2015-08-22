e851548314ac7d51534ec4552fb1a28732bc4fd2
Yuval Adam <yuval@y3xz.com>
2015-08-22 14:54:45 +0300

    Restore nodogsplash with routing repo

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ IMAGE_GENERATOR_URL="https://downloads.openwrt.org/barrier_breaker/14.07/ar71xx/
 tar -xvjf "${IMAGE_GENERATOR_FILENAME}"
 cd "${IMAGE_GENERATOR}"
 
-make image PROFILE=TLMR3040 PACKAGES="" FILES=../files/
+# Add missing routing repo which contains nodogsplash
+sed -i '4isrc/gz barrier_breaker_routing http://downloads.openwrt.org/barrier_breaker/14.07/ar71xx/generic/packages/routing' repositories.conf
+
+make image PROFILE=TLMR3040 PACKAGES="nodogsplash" FILES=../files/
