c7c61f79eaa4a276d3a5daec3d589ad3649f3d4b
Yuval Adam <yuval@y3xz.com>
2015-08-22 14:31:29 +0300

    Update build.sh and remove nodogsplash for now

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+IMAGE_GENERATOR="OpenWrt-ImageBuilder-ar71xx_generic-for-linux-x86_64"
+IMAGE_GENERATOR_FILENAME="${IMAGE_GENERATOR}.tar.bz2"
+IMAGE_GENERATOR_URL="https://downloads.openwrt.org/barrier_breaker/14.07/ar71xx/generic/${IMAGE_GENERATOR_FILENAME}"
 
-wget https://downloads.openwrt.org/barrier_breaker/14.07/ar71xx/generic/OpenWrt-ImageBuilder-ar71xx_generic-for-linux-x86_64.tar.bz2
-tar -xvjf OpenWrt-ImageBuilder-ar71xx_generic-for-linux-x86_64.tar.bz2
-cd OpenWrt-ImageBuilder-ar71xx_generic-for-linux-x86_64
+[ -e "${IMAGE_GENERATOR_FILENAME}" ] || wget "${IMAGE_GENERATOR_URL}"
+tar -xvjf "${IMAGE_GENERATOR_FILENAME}"
+cd "${IMAGE_GENERATOR}"
 
-ln -s ../files .
-
-make image PROFILE=TLMR3040 PACKAGES="nodogsplash" FILES=files/
+make image PROFILE=TLMR3040 PACKAGES="" FILES=../files/
