1462cdb83d06b3f33bcf876c507392a0195730fb
Yuval Adam <yuval@y3xz.com>
2015-08-23 19:33:56 +0300

    Add USB storage support for FAT fs + fstab, fixes #2

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ cd "${IMAGE_GENERATOR}"
 # Add missing routing repo which contains nodogsplash
 sed -i '4isrc/gz barrier_breaker_routing http://downloads.openwrt.org/barrier_breaker/14.07/ar71xx/generic/packages/routing' repositories.conf
 
-make image PROFILE=TLMR3040 PACKAGES="nodogsplash" FILES=../files/
+make image PROFILE=TLMR3040 PACKAGES="block-mount kmod-usb-storage kmod-fs-vfat nodogsplash" FILES=../files/
