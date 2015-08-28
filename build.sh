83c712083effced5c4d42091fbc3c01e2b51c273
Yuval Adam <yuval@y3xz.com>
2015-08-29 01:25:38 +0300

    Image files should be properly branded

--- build.sh
+++ build.sh
@@ -10,4 +10,7 @@ cd "${IMAGE_GENERATOR}"
 # Add missing routing repo which contains nodogsplash
 sed -i '4isrc/gz barrier_breaker_routing http://downloads.openwrt.org/barrier_breaker/14.07/ar71xx/generic/packages/routing' repositories.conf
 
+# Replace filename IMG_PREFIX
+sed -i 's/IMG_PREFIX:=openwrt-$(BOARD)/IMG_PREFIX:=occupywifi-$(BOARD)/' include/image.mk
+
 make image PROFILE=TLMR3040 PACKAGES="block-mount kmod-usb-storage kmod-fs-vfat kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 nodogsplash" FILES=../files/
