582d8a494ef511427972557084ba346e67d22444
Yuval Adam <yuval@y3xz.com>
2015-09-05 12:08:22 +0300

    Add support for TL-MR3020 and TL-WR703 devices

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ IMAGE_GENERATOR="OpenWrt-ImageBuilder-ar71xx_generic-for-linux-x86_64"
 IMAGE_GENERATOR_FILENAME="${IMAGE_GENERATOR}.tar.bz2"
 IMAGE_GENERATOR_URL="https://downloads.openwrt.org/barrier_breaker/14.07/ar71xx/generic/${IMAGE_GENERATOR_FILENAME}"
 
+PROFILES=('TLMR3040' 'TLMR3020' 'TLWR703')
+
 [ -e "${IMAGE_GENERATOR_FILENAME}" ] || wget "${IMAGE_GENERATOR_URL}"
 tar -xvjf "${IMAGE_GENERATOR_FILENAME}"
 cd "${IMAGE_GENERATOR}"
@@ -13,4 +15,7 @@ sed -i '4isrc/gz barrier_breaker_routing http://downloads.openwrt.org/barrier_br
 # Replace filename IMG_PREFIX
 sed -i 's/IMG_PREFIX:=openwrt-$(BOARD)/IMG_PREFIX:=occupywifi-$(BOARD)/' include/image.mk
 
-make image PROFILE=TLMR3040 PACKAGES="block-mount kmod-usb-storage kmod-fs-vfat kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 nodogsplash" FILES=../files/
+for profile in "${PROFILES[@]}"
+do
+	make image PROFILE="$profile" PACKAGES="block-mount kmod-usb-storage kmod-fs-vfat kmod-nls-base kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 nodogsplash uhttpd-mod-lua" FILES=../files/
+done
